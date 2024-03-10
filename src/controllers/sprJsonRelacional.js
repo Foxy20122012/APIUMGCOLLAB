43abbdd5bffa58bf65ff1ca7174b02e0ff393ed5
Foxy20122012 <malvarezm26@miumg.edu.gt>
2024-03-09 22:49:27 -0600

    parseo

--- src/controllers/sprJsonRelacional.js
+++ src/controllers/sprJsonRelacional.js
@@ -27,19 +27,27 @@ export const executeSP = async (spName, params) => {
 };
 
 export const spController = async (req, res) => {
-  const { body } = req;
-
-  if (!body || !body.spName || !body.params || !Array.isArray(body.params)) {
-    return res.status(400).json({ error: 'Solicitud incorrecta' });
-  }
-
-  try {
-    const result = await executeSP(body.spName, body.params);
-    // Asegúrate de enviar solo el primer resultado si tu SP devuelve más de uno.
-    res.status(200).json({ data: result[0] });
-    console.log('Resultado de la ejecución del procedimiento almacenado:', result[0]);
-  } catch (error) {
-    console.error('Error en la API:', error);
-    res.status(500).json({ error: 'Error en la API', details: error.message });
-  }
-};
+    const { body } = req;
+  
+    if (!body || !body.spName || !body.params || !Array.isArray(body.params)) {
+      return res.status(400).json({ error: 'Solicitud incorrecta' });
+    }
+  
+    try {
+      const result = await executeSP(body.spName, body.params);
+      // Asegúrate de enviar solo el primer resultado si tu SP devuelve más de uno.
+      const data = result[0];
+      
+      // Convertir la cadena de 'Temas' en un arreglo de objetos JSON
+      if (data && data.Temas) {
+        data.Temas = JSON.parse(data.Temas);
+      }
+  
+      res.status(200).json({ data: data });
+      console.log('Resultado de la ejecución del procedimiento almacenado:', data);
+    } catch (error) {
+      console.error('Error en la API:', error);
+      res.status(500).json({ error: 'Error en la API', details: error.message });
+    }
+  };
+  
