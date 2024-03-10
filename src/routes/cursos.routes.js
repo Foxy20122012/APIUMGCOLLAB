37cc902caffb3f34fefe4a5779afb7ba8a4ef12a
Foxy20122012 <malvarezm26@miumg.edu.gt>
2024-03-09 23:11:38 -0600

    Corrigiendo metodo de cursos a get

--- src/routes/cursos.routes.js
+++ src/routes/cursos.routes.js
@@ -1,9 +1,9 @@
 import { Router } from 'express';
-import { getCoursesWithTopics } from '../controllers/cursos.controller';
+import { getCoursesWithTopics } from '../controllers/cursos.controller.js';
 
 const router = Router();
 
 // Ejemplo de ruta que utiliza el controlador de data
-router.post('/cursos', getCoursesWithTopics);
+router.get('/cursos', getCoursesWithTopics);
 
 export default router;
