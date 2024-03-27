e033e975c140321bb1b8041faf375695af0de73e
Foxy20122012 <malvarezm26@miumg.edu.gt>
2024-03-26 23:44:07 -0600

    Corrigiendo entorno

--- src/config.js
+++ src/config.js
@@ -7,5 +7,4 @@ export const DB_USER = process.env.DB_USER|| "u930882479_UMGCOLLAB";
 export const DB_PORT = process.env.DB_PORT || "3306";
 export const DB_PASSWORD = process.env.DB_PASSWORD || "Merlos2012";
 export const DB_DATABASE = process.env.DB_DATABASE || "u930882479_UMGCOLLAB";
-export const JWT_SECRET= process.env.JWT_SECRET;
 export const AZURE_STORAGE_CONNECTION_STRING = process.env.AZURE_STORAGE_CONNECTION_STRING;
