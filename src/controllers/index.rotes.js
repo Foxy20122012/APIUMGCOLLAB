2fa269222710762ff652f55c3d46d4579e643ba2
Foxy20122012 <malvarezm26@miumg.edu.gt>
2024-06-12 22:30:00 -0600

    validando ruta del app service. Desploy de prueba

--- src/controllers/index.rotes.js
+++ src/controllers/index.rotes.js
@@ -1,6 +1,7 @@
 export const index = (req, res) => res.json({
   message: "Welcome to UMGCollab API! Dive into our collaborative platform and make the most out of your systems engineering journey.",
   documentation: "https://umgcollab.docs.api",
+  message: "Prueba de app service",
   github: "https://github.com/EnriqueMerlos/UMGCollab"
 });
 
