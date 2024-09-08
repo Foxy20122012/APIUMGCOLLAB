d296874ae84746269466003c8f499808a2acc72e
Foxy20122012 <malvarezm26@miumg.edu.gt>
2024-09-08 02:33:41 -0600

    cambios route news noticias

--- src/routes/admin/categorys/NewsCategory.routes.js
+++ src/routes/admin/categorys/NewsCategory.routes.js
@@ -4,15 +4,15 @@ import { getNewsDetails, addNewsDetail, updateNewsDetail, deleteNewsDetail } fro
 const router = express.Router();
 
 // Obtener todos los detalles de la categoría "Noticias"
-router.get('/newsCategory', getNewsDetails);
+router.get('/categoria/news', getNewsDetails);
 
 // Agregar un nuevo detalle para la categoría "Noticias"
-router.post('/newsCategory', addNewsDetail);
+router.post('/categoria/news', addNewsDetail);
 
 // Actualizar un detalle de la categoría "Noticias"
-router.put('/newsCategory/:id', updateNewsDetail);
+router.put('/categoria/news/:id', updateNewsDetail);
 
 // Eliminar un detalle de la categoría "Noticias"
-router.delete('/newsCategory/:id', deleteNewsDetail);
+router.delete('/categoria/news/:id', deleteNewsDetail);
 
 export default router;
